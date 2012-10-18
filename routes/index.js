dad3dcdf4b2fca6e176910820d316a82ee678b6a
Jen Fong-Adwent <jen@ednapiranha.com>
2012-10-18 17:13:59 -0400

    added stub functions

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,24 @@
 'use strict';
 
 module.exports = function(app, client, isLoggedIn) {
+  var fit = require('../lib/fit');
+
   app.get('/', function(req, res) {
     res.render('index', {
       pageType: 'index',
       session: req.session
+    });
+  });
+
+  app.post('/activity', function(req, res) {
+    fit.addActivity(req, client, function(err, activity) {
+
+    });
+  });
+
+  app.post('/data_point', function(req, res) {
+    fit.addDataPoint(req, client, function(err, dataPoint) {
+
     });
   });
 };
